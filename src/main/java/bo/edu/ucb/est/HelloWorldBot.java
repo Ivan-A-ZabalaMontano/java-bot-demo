264fbf2859b12c742f0958f5c1a74e808b5f6fb4
Ivan-A-ZabalaMontano <ivanzabalamontano@gmail.com>
2021-09-15 22:02:35 -0400

    Telegram bot de suma 1ra version

--- src/main/java/bo/edu/ucb/est/HelloWorldBot.java
+++ src/main/java/bo/edu/ucb/est/HelloWorldBot.java
@@ -28,7 +28,19 @@ public class HelloWorldBot extends TelegramLongPollingBot {
             // Creo el objeto para enviar un mensaje
             SendMessage message = new SendMessage();
             message.setChatId(update.getMessage().getChatId().toString()); //Define a quien le vamos a enviar el mensaje
-            message.setText("Hello " + update.getMessage().getFrom().getFirstName());
+            if(update.getMessage().getText().contains(" y "))
+            {
+            	System.out.println("Mando numeros");
+            	System.out.println("El usuario Mando: "+update.getMessage().getText());
+            	
+            	message.setText(sumarNumeros(update.getMessage().getText()));
+            	
+            }
+            else
+            {
+            	System.out.println("No mando numeros");
+            	message.setText("Hola " + update.getMessage().getFrom().getFirstName()+", para realizar una suma ingresa dos numeros con el siguiente formato: 5 y 3");
+            }
             try {
                 execute(message); // Envia el mensaje
             } catch (TelegramApiException e) {
@@ -36,10 +48,24 @@ public class HelloWorldBot extends TelegramLongPollingBot {
             }
         }
     }
-
     @Override
     public String getBotUsername() {
-        return "ücb_est_bot";
+        return "ISUMA_BOT";
     }
-    
+    public String sumarNumeros(String numeros)
+    {
+    	int n1=0;
+    	int n2=0;
+    	try
+    	{
+    	String[] vec;
+    	vec=numeros.split(" y ");
+    	n1=Integer.parseInt(vec[0]);
+    	n2=Integer.parseInt(vec[1]);
+    	}catch(Exception e)
+    	{
+    		return "Los valores ingresados no son correctos. Vuelva a ingresar los valores con el formato correspondiente por favor";
+    	}
+    	return("La suma es: "+new Suma(n1,n2).realizarSuma());
+    }
 }
